3b91ce3b617f5c00ae5c095dc65bf5fcaff8da1e
Jreng7 <jr.waydev@gmail.com>
2024-10-17 20:25:56 -0300

    user created e list

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  
-  const users = []
 
   const { method, url } = req
 
-  if
+  if(method === 'POST' && url === '/user') {
+
+    users.push({
+      id: 1,
+      name: 'joaquim',
+      email: 'joaquimsilva@gmail.com'
+    })
+
+    return res.writeHead(201).end('User created sucessfull!')
+
+  } else if(method === 'GET' && url === '/list'){
+    return res
+      .setHeader('Content-type', 'aplication/json')
+      .end(JSON.stringify(users))
+  }
+
+  return res.writeHead(404).end('Not Found')
 
 })
 
