e4e823299cdbfbafa222caf445054664bf70985e
Jreng7 <jr.waydev@gmail.com>
2024-10-17 20:50:23 -0300

    criado readable, writable e transform

--- src/front.js
+++ src/front.js
@@ -1,2 +1,35 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
+class One extends Readable {
+  
+  num = 1
+  
+  _read(){
+
+    const i = this.num++
+
+    setTimeout(() => {
+      if(i > 7){
+        this.push(null)
+      } else {
+        const buf = Buffer.from(i.toString())
+        this.push(buf)
+      }
+    }, 300)
+  }
+}
+
+class OneNegative extends Transform {
+  _transform(chunk, encoding, callback){
+    const data = Number(chunk.toString()) * -1
+    callback(null, data.toString())
+  }
+}
+
+class OneTen extends Writable {
+  
+}
+
+new One()
+  .pipe(new OneNegative())
+  .pipe(new OneTen())
