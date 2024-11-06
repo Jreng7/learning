cb97e6b3add69ad1af812cb8d6cd1fadf4da10b8
Jreng7 <jr.waydev@gmail.com>
2024-11-05 23:04:56 -0300

    peso a resultado

--- video_03/script.js
+++ video_03/script.js
@@ -1,4 +1,4 @@
 const nome = document.getElementById('nome')
 const altura = document.getElementById('altura')
-const peso = document.
-const resultado = document.
+const peso = document.getElementById('peso')
+const resultado = document.getElementById('resultado')
