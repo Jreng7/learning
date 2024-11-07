64477cef9aa79fb42c69000319381494a08f327a
Jreng7 <jr.waydev@gmail.com>
2024-11-06 23:07:46 -0300

    reestruturando os documentos by Id

--- video_03/script.js
+++ video_03/script.js
@@ -4,9 +4,9 @@ const calcular = document.getElementById('calcular')
 
 function imc () {
 
-  const nome = document.getElementById('nome')
-  const altura = document.getElementById('altura')
-  const peso = document.getElementById('peso')
+  const nome = document.getElementById('nome').valeu
+  const altura = document.getElementById('altura').value
+  const peso = document.getElementById('peso').value
   const resultado = document.getElementById('resultado')
 
   if (nome !== '') {
