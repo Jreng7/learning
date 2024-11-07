9f0089fc74e0bc8fc76c457c64daa33e53b2bb1b
Jreng7 <jr.waydev@gmail.com>
2024-11-06 22:53:38 -0300

    troca para calcular

--- video_03/script.js
+++ video_03/script.js
@@ -1,4 +1,4 @@
 const nome = document.getElementById('nome')
 const altura = document.getElementById('altura')
 const peso = document.getElementById('peso')
-const resultado = document.getElementById('resultado')
+const calcular = document.getElementById('calcular')
