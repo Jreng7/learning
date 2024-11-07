8d79ff7273b868ce00d8880c23132a27a7190048
Jreng7 <jr.waydev@gmail.com>
2024-11-06 22:54:01 -0300

    fazendo a const resultado

--- video_03/script.js
+++ video_03/script.js
@@ -2,3 +2,4 @@ const nome = document.getElementById('nome')
 const altura = document.getElementById('altura')
 const peso = document.getElementById('peso')
 const calcular = document.getElementById('calcular')
+const resultado = 
