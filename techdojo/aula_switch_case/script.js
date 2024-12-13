a73a9cd2818afb94aeb949821cebee1a6bb4eb21
Jreng7 <jr.waydev@gmail.com>
2024-12-13 20:57:55 -0300

    ajustando código

--- techdojo/aula_switch_case/script.js
+++ techdojo/aula_switch_case/script.js
@@ -1,9 +1,25 @@
-let numberOne = Number(prompt('Digite um valor: '))
+// let numberOne = Number(prompt('Digite um valor: '))
 
-if ( numberOne > 0) {
-  alert(`O número ${numberOne} é positivo`)
-} else if (numberOne < 0 ){
-  alert(`O número ${numberOne} negativo.`)
-} else {
-  alert(`O número ${numberOne} é neutro.`)
+// if ( numberOne > 0) {
+//   alert(`O número ${numberOne} é positivo`)
+// } else if (numberOne < 0 ){
+//   alert(`O número ${numberOne} negativo.`)
+// } else {
+//   alert(`O número ${numberOne} é neutro.`)
+// }
+
+const turno = prompt("Digite o turno atual: ")
+
+switch(turno.toLowerCase()){  // Switch fica "vigiando" a variável turno e transforma o valor recebido em letras minúsculas.
+  case "dia":
+    alert("Bom dia");
+    break;
+  case "tarde":
+    alert("Boa tarde");
+    break;
+  case "noite":
+    alert("Boa noite");
+    break;
+  default:
+    alert("Nada foi digitado.");
 }
