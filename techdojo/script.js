f997ba2950f1ae07d76261fa98d744f0c26bc954
Jreng7 <jr.waydev@gmail.com>
2024-12-13 19:48:47 -0300

    testando prompt

---
+++ techdojo/script.js
@@ -0,0 +1,4 @@
+const numero1 = prompt("Digite um número: ")   // Captura no console o valor digitado pelo usuário.
+console.log(typeof numero1) // Em seguida mostra o valor e o tipo do valor. No caso é do tipo String.
+
+alert(`O número digitado foi: ${numero1}`)
