b8cf9760dca4276deedaeb3c639597aa3b7996ef
Jreng7 <jr.waydev@gmail.com>
2024-12-13 20:44:29 -0300

    reestruturando else if

---
+++ techdojo/aula_switch_case/script.js
@@ -0,0 +1,9 @@
+let numberOne = Number(prompt('Digite um valor: '))
+
+if ( numberOne > 0) {
+  alert(`O número ${numberOne} é positivo`)
+} else if (numberOne < 0 ){
+  alert(`O número ${numberOne} negativo.`)
+} else {
+  alert(`O número ${numberOne} é neutro.`)
+}
