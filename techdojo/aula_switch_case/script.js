74a503fe371a9180da848425743bbcdd54652ac1
Jreng7 <jr.waydev@gmail.com>
2024-12-13 21:04:29 -0300

    Comparando Switch Case com Else if

--- techdojo/aula_switch_case/script.js
+++ techdojo/aula_switch_case/script.js
@@ -8,6 +8,8 @@
 //   alert(`O número ${numberOne} é neutro.`)
 // }
 
+
+// Com Switch Case
 const turno = prompt("Digite o turno atual: ")
 
 switch(turno.toLowerCase()){  // Switch fica "vigiando" a variável turno e transforma o valor recebido em letras minúsculas.
@@ -22,4 +24,18 @@ switch(turno.toLowerCase()){  // Switch fica "vigiando" a variável turno e tran
     break;
   default:
     alert("Nada foi digitado.");
+}
+
+
+// Com if else 
+const turno2 = prompt("Digite o turno atual: ").toLowerCase()
+
+if(turno2 === 'dia'){
+  alert("Bom dia")
+} else if (turno2 === 'tarde') {
+  alert("Boa tarde")
+} else if (turno2 === 'noite') {
+  alert("Boa noite")
+} else {
+  alert("Nenhum turno foi digitado.")
 }
