195cad1816f50da84126e8ea52677e1085beadde
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-01 11:31:23 -0300

    types node and express

--- udemy_derci_nodejs/src/index.js
+++ udemy_derci_nodejs/src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Bem-vindo ao meu site!");
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
