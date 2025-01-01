621d1ac7dcdef350c59ecc8c035b1694df10f291
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-01 20:04:59 -0300

    app.get

---
+++ udemy_derci_nodejs/lib/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Bem-vindo ao meu site! ok 2");
+});
+let id = 0;
+const users = [];
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+app.get("/users/:id", (req, res) => {
+});
+app.post("/users", (req, res) => {
+    let user = req.body;
+    user.id = ++id;
+    users.push(user);
+    res.send({ message: "usuário criado com sucesso." });
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+//# sourceMappingURL=index.js.map
--- udemy_derci_nodejs/src/index.ts
+++ udemy_derci_nodejs/src/index.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bem-vindo ao meu site!");
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bem-vindo ao meu site! ok 2");
+})
+
+let id = 0;
+const users: {id: number, nome: string, email: string}[] = [];
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send(users);
+})
+
+app.get("/users/:id", (req: Request, res: Response) => {
+  
+})
+
+app.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+  user.id = ++id;
+  users.push(user)
+  res.send({ message: "usuário criado com sucesso." })
 })
 
 app.listen(3000, () => {
