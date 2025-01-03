acb83be34e1e4dd261ad9006314459f4f4d291f1
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-02 22:38:27 -0300

    arrumando tipo User

--- udemy_derci_nodejs/src/index.ts
+++ udemy_derci_nodejs/src/index.ts
@@ -4,45 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Bem-vindo ao meu site! ok 2");
-})
 
-
-
-
-
-app.get("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = users.find(item => item.id === userId)
-  res.send(user);
-})
-
-app.post("/users", (req: Request, res: Response) => {
-  let user = req.body;
-  user.id = ++id;
-  users.push(user)
-  res.send({ message: "usuário criado com sucesso." })
-})
-
-app.put("/users/:id", (req: Request, res: Response) => {
-
-  let userId = Number(req.params.id);
-  let user = req.body;
-  let indexOf = users.findIndex((_user: User) => _user.id === userId);
-  users[indexOf].name = user.name;
-  users[indexOf].email = user.email;
-
-  res.send({messagem: "Usuário alterado com sucesso."})
-
-})
-
-app.delete("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let indexOf = users.findIndex((_user: User) => _user.id === userId);
-  users.splice(indexOf, 1);
-  res.send({message: "Usuário excluído com sucesso."})
-})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- udemy_derci_nodejs/src/routes/users.route.ts
+++ udemy_derci_nodejs/src/routes/users.route.ts
@@ -3,11 +3,63 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 
-type User = [{id: number, name: string, email: string}]
+type User = {
+  
+  id: number;
+  name: string; 
+  email: string;
+
+}
+
 let id = 0;
 const users: User[] = []
 
 
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Bem-vindo ao meu site! ok 2");
+})
+
+
+// METODO GET
 router.get("/users", (req: Request, res: Response) => {
   res.send(users);
+})
+
+
+// METODO GET
+router.get("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = users.find(item => item.id === userId)
+  res.send(user);
+})
+
+
+// METODO POST
+router.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+  user.id = ++id;
+  users.push(user)
+  res.send({ message: "usuário criado com sucesso." })
+})
+
+// METODO PUT
+router.put("/users/:id", (req: Request, res: Response) => {
+
+  let userId = Number(req.params.id);
+  let user = req.body;
+  let indexOf = users.findIndex((user: User) => user.id === userId);
+  users[indexOf].name = user.nameBody;
+  users[indexOf].email = user.emailBody;
+
+  res.send({messagem: "Usuário alterado com sucesso."})
+
+})
+
+// METODO DELETE
+router.delete("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let indexOf = users.findIndex((user: User) => user.id === userId);
+  users.splice(indexOf, 1);
+  res.send({message: "Usuário excluído com sucesso."})
 })
