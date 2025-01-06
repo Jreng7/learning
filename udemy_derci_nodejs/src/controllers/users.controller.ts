03b4c3c573f929a7b1cbbba0ff6ffea3374ba29d
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-05 22:39:41 -0300

    update controller

--- udemy_derci_nodejs/src/controllers/users.controller.ts
+++ udemy_derci_nodejs/src/controllers/users.controller.ts
@@ -11,13 +11,43 @@ const users: User[] = []
 
 export class UsersController {
 
+  // metodo get - Pegar todos os usuários
   static getAllUsers(req: Request, res: Response){
     res.send(users)
   }
 
+  // metodo get - Pegar usuário pelo Id
   static getUserById(req: Request, res: Response){
     let userId = Number(req.params.id)
     let user = users.find((usuario: User) => usuario.id === userId)
     res.send(user)
-  
+  }
+
+  // metodo post - inserir usuaŕios.
+  static insertUser(req: Request, res: Response) {
+    let user = req.body
+    user.id = ++id
+    users.push(user)
+    res.send({message: "Usuário criado com sucesso!"})
+  }
+
+  // metodo put - atualizar usuário
+  static updateUserById(req: Request, res: Response){
+    let userId = Number(req.params.id)
+    let user = req.body
+    let indexOf = users.findIndex((usuarioUpdate: User) => usuarioUpdate.id === userId)
+    users[indexOf].nome = user.nome
+    user[indexOf].email = user.email
+    res.send({message: "Usuário atualizado com sucesso!"})
+  }
+
+  // Metodo delete
+  static deleteUserById(req: Request, res: Response){
+    let userId = Number(req.params.id)
+    let findUser = users.findIndex((user: User) => user.id === userId)
+    users.splice(findUser, 1)
+    res.send({message: "Usuário deletado com sucesso."})
+  }
+
 }
+
--- udemy_derci_nodejs/src/routes/users.route.ts
+++ udemy_derci_nodejs/src/routes/users.route.ts
@@ -1,40 +1,13 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { UsersController } from '../controllers/users.controller' 
 
 export const userRoutes = express.Router();
 
 
-
 userRoutes.get("/users", UsersController.getAllUsers)
-
-
-// Metodo get pega usuário por 
 userRoutes.get("/users/:id", UsersController.getUserById)
-
-// Metodo post
-userRoutes.post("/users", (req: Request, res: Response) => {
-  let user = req.body
-  user.id = ++id
-  users.push(user)
-  res.send({message: "Usuário criado com sucesso!"})
-})
-
-// Metodo put
-userRoutes.put("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id)
-  let user= req.body
-  let indexOf = users.findIndex((usuarioPercorrido: User) => usuarioPercorrido.id === userId)
-  users[indexOf].nome = user.nome
-  users[indexOf].email = user.email
-  res.send({message: "Usuário atualizado com sucesso."})
-})
-
-// delete
-userRoutes.delete("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id)
-  let user = req.body
-  users.splice(user, userId)
-  res.send({message: "Usuário deletado com sucesso."})
-})
+userRoutes.post("/users", UsersController.insertUser)
+userRoutes.put("/users/:id", UsersController.updateUserById)
+userRoutes.delete("/users/:id", UsersController.deleteUserById)
 
 
