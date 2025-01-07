5f3de9cee0983156ab764302da87d158d43ea4e9
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-06 21:59:52 -0300

    primeiro projeto com next.js do curso novo programador

---
+++ nv-programador_react/src/layouts/Header.tsx
@@ -0,0 +1,14 @@
+
+type HeaderProps = {
+  nomeDoCliente?: string;
+}
+
+export function Header(props: HeaderProps){
+  return (
+    <>
+      <header>
+        <h1>{props.nomeDoCliente}</h1>
+      </header>
+    </>
+  )
+}
