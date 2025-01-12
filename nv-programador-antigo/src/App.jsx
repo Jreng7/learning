39b7a68fcec46326fdacd6f4af1d11679b21133a
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-12 00:01:20 -0300

    useState

--- nv-programador-antigo/src/App.jsx
+++ nv-programador-antigo/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react"
 
-function App() {
-  const [login, setLogin] = useState(false)
+import { useState } from 'react';
+
+export default function App() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
 
   return (
     <>
-      <button onClick={() => setLogin(true)}></button>
+      <form>
+        <p>Nome:</p>
+        <input type="text" onChange={(e) => {setNome(e.target.value)}}/>
+        
+        <br />
+
+        <p>E-mail:</p>
+        <input type="email" onChange={(e) => {setEmail(e.target.value)}}/>
+
+        <br />
+
+        <p>Mensagem</p>
+        <textarea placeholder="Digite sua mensagem:" onChange={(e) => {setMensagem(e.target.value)}}></textarea>
+
+        <br /><br />
+        <button type="submit">Enviar</button>
+      </form>
     </>
   )
 }
-
-export default App
