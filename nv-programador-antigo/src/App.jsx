de3b9dbcbda9cf189ece9393df3e66a8c425bef2
Jreng7 <jr.waydev@gmail.com>
2025-01-16 21:27:47 -0300

    update form

--- nv-programador-antigo/src/App.jsx
+++ nv-programador-antigo/src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 
 export default function App() {
 
-  const [nome, setNome] = useState('Josué Ribeiro');
+  const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [mensagem, setMensagem] = useState('');
 
   function handleSubmit(e){
     e.preventDefault();
+    console.log(nome, email, mensagem);
   }
 
   return (
