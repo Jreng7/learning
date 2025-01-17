c4fd7f3f6021f70142223963151259b763c303a7
Jreng7 <jr.waydev@gmail.com>
2025-01-16 23:21:08 -0300

    handleSubmit

--- nv-programador-antigo/src/App.jsx
+++ nv-programador-antigo/src/App.jsx
@@ -7,14 +7,28 @@ export default function App() {
   const [num2, setNum2] = useState(0);
   const [resultado, setResultado] = useState(0);
 
+  function handleSubmit(e){
+    e.preventDefault();
+
+    switch (operacao) {
+      case '+':
+        setResultado(num1 + num2)
+        break;
+
+      default:
+        break;
+    }
+
+  }
+
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
 
-        <input type="number" onChange={(e) => {setNum1(e.target.value)}}/>
+        <input type="number" onChange={(e) => {setNum1(e.target.value)}} value={num1}/>
 
-        <select>
+        <select onChange={(e) => {setOperacao(e.target.value)}} value={operacao}>
           <option value="">--Selecione a operação--</option>
           <option value="+">+</option>  
           <option value="-">-</option>
@@ -22,8 +36,8 @@ export default function App() {
           <option value="*">*</option>
         </select> 
 
-        <input type="number" />
-        <button type="submit">Calcular</button><b>Resultado:</b>{resultado}
+        <input type="number" onChange={(e) => {setNum2(e.target.value)}} value={num2}/>
+        <button type="submit" onChange={(e) => {setResultado(e.target.value)}}>Calcular</button><b>Resultado:</b>{resultado}
 
       </form>
     </>
