00767fe608d383b70424bf9b198aa6782a021145
Jr.eng7 <jr.waydev@gmail.com>
2025-01-20 17:32:52 -0300

    trocando ID

--- nv-programador-node.js/src/index.ts
+++ nv-programador-node.js/src/index.ts
@@ -6,9 +6,9 @@ server.get("/", (req: Request, res: Response) => {
   return res.json({message: "Testando servidor."})
 })
 
-server.get("/criancas/:id_parametro", (req: Request, res: Response) => {
-  const id_pijamas = req.params.id_parametro
-  return res.json({message: "Pijama de bolinhas azuis."})
+server.get("/criancas/:id", (req: Request, res: Response) => {
+  const id_pijamas = req.params.id
+  return res.json({id: id_pijamas, message: "Pijama de bolinhas azuis."})
 })
 
 server.listen(3000);
