daeddf9468866028526599029a06198b6676065a
Jr.eng7 <jr.waydev@gmail.com>
2025-01-20 13:13:54 -0300

    get teste

--- nv-programador-node.js/src/index.ts
+++ nv-programador-node.js/src/index.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { request, Request, Response } from 'express';
 
 const server = express();
 
 server.get("/", (req: Request, res: Response) => {
   return res.json({message: "Testando servidor."})
+})
+
+server.get("/criancas/:id_pijamas", (req: Request, res: Response) => {
+  const params = req.params
+  return res.json({message: "Pijama de bolinhas azuis."})
 })
 
 server.listen(3000);
