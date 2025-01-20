403e0aa9f992a51494ec2e431c75651c9cda6f6d
Jreng7 <jr.waydev@gmail.com>
2025-01-19 23:36:12 -0300

    ajustando versão do typescript

--- nv-programador-node.js/src/server.ts
+++ nv-programador-node.js/src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const server = express()
+const server = express();
 
 server.get("/", (req: Request, res: Response) => {
-  
+  return res.json({message: "Testando servidor."})
 })
 
-server.listen(3000)
+server.listen(3000);
