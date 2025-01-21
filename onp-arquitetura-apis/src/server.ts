681f6fd5c307161c4b4735303edde9da6cc6d93f
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 15:26:25 -0300

    routes criado com sucesso

--- onp-arquitetura-apis/src/server.ts
+++ onp-arquitetura-apis/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { router } from '../routes';
 
 dotenv.config()
 
@@ -8,7 +9,7 @@ const server = express();
 
 server.use(cors())
 server.use(express.json())
-
+server.use(router)
 
 
 server.listen(process.env.PORT)
