0e817032905430d84a6a534984077f5fa4096ce8
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 20:34:15 -0300

    finish

---
+++ onp-arquitetura-apis/src/repositories/AlunoRepository.ts
@@ -0,0 +1,21 @@
+import { IAluno } from "../models/aluno"
+
+export class AlunoRepository {
+
+  getAll(){
+      
+    }
+  
+    add(data: IAluno){
+     
+    }
+  
+    update(id: string) {
+      
+    }
+  
+    delete(id: string){
+     
+    }
+
+}
