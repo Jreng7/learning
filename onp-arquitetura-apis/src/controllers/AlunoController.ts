12391c31f6027987392ed8e6eaf476a347079a1d
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 18:22:05 -0300

    alunoservice

--- onp-arquitetura-apis/src/controllers/AlunoController.ts
+++ onp-arquitetura-apis/src/controllers/AlunoController.ts
@@ -1,33 +1,33 @@
-
 import { Request, Response } from 'express'
+import { AlunoService } from '../services/AlunoService'
 
+const alunoService = new AlunoService()
 
 export class AlunoController {
   
   //  Buscar todos os alunos.
   getAllStudents(req: Request, res: Response){
-    return res.json({message: "Alunos listados com sucesso pela classe!"})
+    const result = alunoService.getAll()
   }
 
   //  Buscar um aluno por ID.
   getStudentById(req: Request, res: Response){
-    return res.json({message: "Busca por um aluno"})
+    const result = alunoService.getId(req.params.id)
   }
 
   //  Adicionar um aluno.
   addStudent(req: Request, res: Response){
-    return res.json({message: "Aluno adicionado com sucesso!"})
+    const result = alunoService.add(req.body)
   }
 
   //  Editar um aluno.
   updateStudent(req: Request, res: Response){
-    return res.json({message: "Atualizar um aluno"})
+    const result = alunoService.update(req.params.id)
   }
 
   //  Excluir um aluno.
   deleteStudent(req: Request, res: Response){
-    return res.json({message: "Aluno deletado com sucesso!"})
+    const result = alunoService.delete(req.params.id)
   }
-
 
 }
---
+++ onp-arquitetura-apis/src/services/AlunoService.ts
@@ -0,0 +1,8 @@
+
+export class AlunoService {
+  getAll(){}
+  getId(id: string){}
+  add(data: any){}
+  update(id: string){}
+  delete(id: string){}
+}
