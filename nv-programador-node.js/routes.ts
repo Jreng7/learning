ba1d07f1881a6ec96cf3769be6897d486ce3f310
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 00:41:30 -0300

    estruturando params e params.id

--- nv-programador-node.js/routes.ts
+++ nv-programador-node.js/routes.ts
@@ -1,7 +1,7 @@
-import { Request, Response, Router } from 'express'
+import { Request, Response, Router } from 'express';
 
 
-export const router = Router()
+export const router = Router();
 
 
  // Metodo GET
@@ -10,18 +10,24 @@ router.get("/criancas/:id", (req: Request, res: Response) => {
   const { tamanho, cor } = req.query
 
   return res.json({id_pijamas, tamanho, cor})
-})
+});
 
   // Metodo POST
 router.post('/painel/pijama', (req: Request, res: Response) => {
   const { nome, tamanho, cor, preco, quantidade_estoque } = req.body;
 
   return res.json({nome, tamanho, cor, preco, quantidade_estoque})
-})
+});
 
   // Metodo PUT
-router.put('/painel/pijama', (req: Request, res: Response) => {
+router.put('/painel/pijama/:id', (req: Request, res: Response) => {
   const { nome, tamanho, cor, preco, quantidade_estoque } = req.body;
+  const { id } = req.params
+  const { authorization } = req.headers;
+
+  if(!authorization) {
+    return res.status(400).json({error: "Authorization is required"})
+  }
   
-  return res.json({nome, tamanho, cor, preco, quantidade_estoque})
-})
+  return res.json({id, metodo: 'PUT', nome, tamanho, cor, preco, quantidade_estoque, authorization})
+});
--- nv-programador-node.js/src/index.ts
+++ nv-programador-node.js/src/index.ts
@@ -1,11 +1,18 @@
-import express from 'express';
-import { router } from '../routes'
+import express, { Request, Response } from 'express';
+import { router } from '../routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
 
+server.use(express.urlencoded({extended: true}));
+server.use(router);
 
-server.use(express.urlencoded({extended: true}))
-server.use(router)
+server.use((req: Request, res: Response) => {
+  res.status(404).json({error: "Route not found."})
+})
 
 
-server.listen(3333);
+server.listen(process.env.PORT);
+
