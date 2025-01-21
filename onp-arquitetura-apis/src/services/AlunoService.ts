2c11f1b637d9faddee613ebb127ab4f966ff013c
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 19:36:00 -0300

    getId

--- onp-arquitetura-apis/src/services/AlunoService.ts
+++ onp-arquitetura-apis/src/services/AlunoService.ts
@@ -6,8 +6,8 @@ export class AlunoService {
     return [{name: "Joaquim", email: "joaq@gmail.com", phone: "351", password: "1587", cpf: "1587"}]
   }
 
-  getId(id: string){
-
+  getId(id: string): IAluno {
+    return {name: "Joaquim", email: "joaq@gmail.com", phone: "351", password: "1587", cpf: "1587"}
   }
 
   add(data: IAluno): IAluno {
