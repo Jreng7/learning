97d82c28b1a5feb2da3a75a207e6c041d4eb6f23
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 18:57:06 -0300

    criando, exportando e importando interface Aluno

---
+++ onp-arquitetura-apis/src/models/aluno.ts
@@ -0,0 +1,9 @@
+
+
+export interface IAluno {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  cpf: string;
+}
--- onp-arquitetura-apis/src/services/AlunoService.ts
+++ onp-arquitetura-apis/src/services/AlunoService.ts
@@ -1,8 +1,25 @@
+import { IAluno } from "../models/aluno";
 
 export class AlunoService {
-  getAll(){}
-  getId(id: string){}
-  add(data: any){}
-  update(id: string){}
-  delete(id: string){}
+  
+  getAll(){
+
+  }
+
+  getId(id: string){
+
+  }
+
+  add(data: IAluno){
+
+  }
+
+  update(id: string){
+
+  }
+
+  delete(id: string){
+
+  }
+
 }
