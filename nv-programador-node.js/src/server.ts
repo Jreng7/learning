8df860f0f82a1cf2aa2b3a3b53d93d03cfcf3779
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 10:53:50 -0300

    instalado cors

--- nv-programador-node.js/src/server.ts
+++ nv-programador-node.js/src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
 import { router } from '../routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const server = express();
 
 server.use(express.urlencoded({extended: true}));
+server.use(cors())
 server.use(router);
 
 server.use((req: Request, res: Response) => {
