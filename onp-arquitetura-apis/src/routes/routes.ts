2af6697274df764c55d149bb1f29f2bf35b7d9d9
Jr.eng7 <jr.waydev@gmail.com>
2025-01-24 00:55:33 -0300

    tratando erro no middlewares

--- onp-arquitetura-apis/src/routes/routes.ts
+++ onp-arquitetura-apis/src/routes/routes.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Router } from 'express'
+import { NextFunction, Router, Request, Response } from 'express'
 import { AlunoController } from '../controllers/AlunoController'
 
 export const router = Router()
@@ -7,11 +7,20 @@ const alunoController = new AlunoController();
 
 
 const authMiddleware = ( req: Request, res: Response, next: NextFunction ) => {
-  
+
+  // Executar as verificações necessárias 
+
+  if ( req.headers.authorization ) {
+    // validar o token para identificar a existência do usuário
+    next();
+  } else {
+    return res.status(401).json({ error: 'Usuário não autenticado' });
+  }
+
 }
 
 
-router.get("/aluno", alunoController.getAllStudents) //  Buscar todos os alunos.
+router.get("/aluno", authMiddleware, alunoController.getAllStudents) //  Buscar todos os alunos.
 router.get("/aluno/:id", alunoController.getStudentById) //  Buscar um aluno por ID.
 router.post("/aluno", alunoController.addStudent)  //  Adicionar um aluno.
 router.put("/aluno/:id",alunoController.updateStudent)  //  Editar um aluno.
