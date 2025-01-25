1e87887ffd3c53862757258a84ec32fb028304a1
Jr.eng7 <jr.waydev@gmail.com>
2025-01-25 00:15:42 -0300

    gerando id único

--- support-ticket-rt/src/controllers/tickets/create.js
+++ support-ticket-rt/src/controllers/tickets/create.js
@@ -5,7 +5,7 @@ export function create({ req, res }){
   const { equipment, description, user_name } = req.body
 
   const ticket = {
-    id: ,
+    id: randomUUID(),
     equipment,
     description,
     user_name
