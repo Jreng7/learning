fcf9db51f72d8b4f375f7cde1c6a1e815d31ca6d
Jr.eng7 <jr.waydev@gmail.com>
2025-01-27 14:27:00 -0300

    export class

---
+++ full-stack-rocketseat/proj-api-rest/src/controllers/ProductsController.ts
@@ -0,0 +1,16 @@
+
+/*
+  
+  getAll => GET para listar vários registros.
+  show => GET para exibir um registro específico.
+  create => POST para criar um registro.
+  update => PUT para atualizar um registro.
+  remove => DELETE para deletar um registro.  */
+
+
+export class ProductsController {
+
+  
+
+}
+
