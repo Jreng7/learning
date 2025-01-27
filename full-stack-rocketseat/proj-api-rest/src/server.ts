66b1bbd78804d41361f8e5c0ba78adf04b592186
Jr.eng7 <jr.waydev@gmail.com>
2025-01-27 11:13:25 -0300

    criando post

--- full-stack-rocketseat/proj-api-rest/src/server.ts
+++ full-stack-rocketseat/proj-api-rest/src/server.ts
@@ -1,7 +1,20 @@
 import express from 'express'
 
-
 const server = express()
+
+
+server.get('/products', (request, response) => {
+  const { page, limit } = request.query
+  response.send(`As query params são "${page}" e "${limit}"`)
+})
+
+
+server.post('/products', (request, response) => {
+  
+})
+
+
+
 
 const PORT = 3333
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
