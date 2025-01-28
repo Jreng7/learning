640d8ee20f577b881d8ca98acb395136caee6dd2
Jr.eng7 <jr.waydev@gmail.com>
2025-01-28 20:15:30 -0300

    example page component

---
+++ nv-programador_next/first-next-app/src/app/example/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useState } from 'react'
+
+import ButtonChatOnline from '../components/ButtonChatOnline'
+
+export function Example(){
+  const [cor, setCor] = useState("blue")
+}
