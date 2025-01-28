b520ab4eb701561647f6cd89f02584bc367969af
Jr.eng7 <jr.waydev@gmail.com>
2025-01-27 23:55:35 -0300

    trim, min no zod

--- full-stack-rocketseat/proj-api-rest/src/controllers/ProductsController.ts
+++ full-stack-rocketseat/proj-api-rest/src/controllers/ProductsController.ts
@@ -27,8 +27,8 @@ export class ProductsController {
 
     const bodySchema = z.object({
 
-      name: z.string(),
-      price: z.number()
+      name: z.string({ required_error: "Name is required" }).trim().min(4, { message: "Name must be 4 or more characteres"}),
+      price: z.number({ required_error: "Price is required" })
 
     })
 
