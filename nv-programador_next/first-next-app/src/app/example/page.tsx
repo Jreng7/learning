956a4a2adc9727f80d2aed187e3a00ed16572c23
Jr.eng7 <jr.waydev@gmail.com>
2025-01-28 20:16:19 -0300

    useState para mudança de cor

--- nv-programador_next/first-next-app/src/app/example/page.tsx
+++ nv-programador_next/first-next-app/src/app/example/page.tsx
@@ -5,5 +5,14 @@ import { useState } from 'react'
 import ButtonChatOnline from '../components/ButtonChatOnline'
 
 export function Example(){
+
   const [cor, setCor] = useState("blue")
+
+  function alertar(){
+    setCor("red")
+  }
+
+  return(
+    
+  )
 }
