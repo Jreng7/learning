c3ff08a38dd27a26e74d103281cd1aaf101d1d95
Jreng7 <jr.waydev@gmail.com>
2025-02-01 13:02:14 -0300

    rota update (put)

--- full-stack-rocketseat/query_build_project/src/server.ts
+++ full-stack-rocketseat/query_build_project/src/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express"
 import { knex } from "./database/knex"
 
+
 const server = express()
 server.use(express.json())
+
 
 server.post("/courses", async (request: Request, response: Response) => {
 
@@ -12,5 +14,23 @@ server.post("/courses", async (request: Request, response: Response) => {
 
   response.status(201).json()
 })
+
+
+server.get("/courses", async (request: Request, response: Response) => {
+
+  const courses = await knex("courses").select().orderBy("name")
+
+  response.json({ courses })
+})
+
+
+server.put("/courses", async (request: Request, response: Response) => {
+
+  const courses = await knex("courses").select().orderBy("name")
+
+  response.json({ courses })
+})
+
+
 
 server.listen(3333, () => console.log(`Ok! Server is running on port 3333`))
