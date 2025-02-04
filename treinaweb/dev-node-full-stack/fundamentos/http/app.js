3c1e1d42413349683038b2785a4dfe0c2c2b6446
Jreng7 <jr.waydev@gmail.com>
2025-02-04 07:55:13 -0300

    fazendo else if para http puro

--- treinaweb/dev-node-full-stack/fundamentos/http/app.js
+++ treinaweb/dev-node-full-stack/fundamentos/http/app.js
@@ -1,9 +1,30 @@
 import { createServer }from 'node:http'
 
+// const server = createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html'}),
+//   res.write('<h1>TreinaWeb</h1>'),
+//   res.end()
+// })
+
+// server.listen(3333, () => {
+//   console.log('Server is running on port 3333')
+// })
+
+
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html'}),
-  res.write('<h1>TreinaWeb</h1>'),
-  res.end()
+  
+  res.writeHead(200, { 'Content-Type': 'text/html'})
+  
+  let url = req.url
+
+  if( url  === '/sobre' ) {
+
+    res.write('<h1>TreinaWeb Sobre</h1>')
+    res.end()
+  } else if (url === '/contato') {
+    res.write('<h1>TreinaWeb Contato</h1>')
+  }
+
 })
 
 server.listen(3333, () => {
