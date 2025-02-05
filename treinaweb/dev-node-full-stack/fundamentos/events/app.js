8aed622a0a2e21b803c050fe32e032f7e95eb796
Jreng7 <jr.waydev@gmail.com>
2025-02-04 22:56:29 -0300

    app.js

--- treinaweb/dev-node-full-stack/fundamentos/events/app.js
+++ treinaweb/dev-node-full-stack/fundamentos/events/app.js
@@ -1,4 +1,27 @@
 import EmmitEvents from 'node:events'
 
-const emitter = new EmmitEvents()
+// const emitter = new EmmitEvents()
 
+// emitter.on('evento', () => {
+//   console.log('Evento acionado')
+// })
+
+// emitter.emit('evento')
+
+class Cachorro extends EmmitEvents {
+  latir(){
+    console.log("Latindo...")
+  }
+}
+
+let rex = new Cachorro()
+
+//rex.on('pessoa_na_porta', rex.latir) Executa muitas vezes
+rex.once('pessoa_na_porta', rex.latir) // executa apenas uma vez.
+rex.emit('pessoa_na_porta')
+rex.emit('pessoa_na_porta')
+rex.emit('pessoa_na_porta')
+rex.emit('pessoa_na_porta')
+rex.removeListener('pessoa_na_porta', rex.latir)
+rex.emit('pessoa_na_porta')
+rex.emit('pessoa_na_porta')
