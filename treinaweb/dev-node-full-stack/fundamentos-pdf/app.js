a97eae17c69f7f797b051e189dead6aee2cb69a9
Jreng7 <jr.waydev@gmail.com>
2025-02-04 23:37:28 -0300

    não foi possível concluir a aula, pois o pacote html-pdf está desatualizado

--- treinaweb/dev-node-full-stack/fundamentos-pdf/app.js
+++ treinaweb/dev-node-full-stack/fundamentos-pdf/app.js
@@ -1,18 +1,34 @@
-import { createServer } from 'node:http'
-import fs from 'node:fs'
-import pdf from 'html-pdf'
+import * as http from 'node:http'
+import * as fs from 'node:fs'
+import * as pdf from 'html-pdf'
 
 
 
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
 
-  let html = fs.readFileSync('./index.html')
+  let html = fs.readFileSync('./index.html', 'utf8')
 
   const options = {
-    type: 'pdf',
+    type: 'pdf',  
     format: 'A4',
     orientation: 'portrait'
   }
 
+  pdf.create(html.toString(), options).toBuffer((err, buffer) => {
 
+    if(err) {
+      console.error("Erro ao criar o PDF:", err)
+      res.writeHead(500, {'Content-Type': 'text/plain'})
+      res.end()
+    }
+
+    res.writeHead(200, {'Content-Type': 'application/pdf'})
+    res.end(buffer)
+
+  })
+
+})
+
+server.listen(3000, () => {
+  console.log('Servidor iniciado com sucesso!')
 })
