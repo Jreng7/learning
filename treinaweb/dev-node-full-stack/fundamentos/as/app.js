e5295b296ce6e4809453eae33c126df2f2dfaa95
Jreng7 <jr.waydev@gmail.com>
2025-02-05 21:27:45 -0300

    terminando a lógica

--- treinaweb/dev-node-full-stack/fundamentos/as/app.js
+++ treinaweb/dev-node-full-stack/fundamentos/as/app.js
@@ -6,9 +6,19 @@ console.time('Assíncrono')
 let counter = 0
 
 for(let i = 0; i < 500; i++){
+
   fs.readFile('meu_arquivo.txt', (err, data) => {
-    if(err){
-      return console.error
+
+    if(err) { 
+      return console.error(err)
     }
+    counter++
+    console.log('Assíncrono' + data.toString())
+
+    if(counter === 500) {
+      console.timeEnd('Termino do Assíncrono')
+    }
+
   })
+
 }
