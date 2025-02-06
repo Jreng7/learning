112acb326eea3a1c2b31079681ade7443e83dee1
Jreng7 <jr.waydev@gmail.com>
2025-02-05 21:40:39 -0300

    fs com readFile

--- treinaweb/dev-node-full-stack/fundamentos/as/app.js
+++ treinaweb/dev-node-full-stack/fundamentos/as/app.js
@@ -1,22 +1,32 @@
 import fs from 'node:fs'
 
-console.time('Assíncrono')
+console.time('Time Assíncrono')
 
+
+fs.writeFile('meu_arquivo.txt', 'TreinaWeb', (err) => {
+
+  if(err) {
+    return console.error('Erro ao escrever o arquivo', err)
+  }
+
+  console.log('Arquivo escrito com sucesso!')
+
+})
 
 let counter = 0
 
-for(let i = 0; i < 500; i++){
+for(let i = 0; i < 20; i++){
 
   fs.readFile('meu_arquivo.txt', (err, data) => {
 
-    if(err) { 
+    if(err) {
       return console.error(err)
     }
     counter++
     console.log('Assíncrono' + data.toString())
 
-    if(counter === 500) {
-      console.timeEnd('Termino do Assíncrono')
+    if(counter === 20) {
+      console.timeEnd('Time Assíncrono')
     }
 
   })
