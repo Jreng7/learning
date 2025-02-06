153acd6cbb9c32e9aed95d70d79ad0e98f2dcda2
Jreng7 <jr.waydev@gmail.com>
2025-02-06 19:52:00 -0300

    productsController

---
+++ full-stack-rocketseat/api-restaurant/src/controllers/products-controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+
+export class ProductController {
+
+  async index(request: Request, response: Response, next: NextFunction){
+    try {
+      return response.json({ message: "Ok"})
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- full-stack-rocketseat/api-restaurant/src/routes/products-routes.ts
+++ full-stack-rocketseat/api-restaurant/src/routes/products-routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express'
+import { ProductController } from '@/controllers/products-controller.js'
+
 
 export const productsRoutes = Router()
+const productController = new ProductController()
 
+
+productsRoutes.get('/', productController.index)
 
