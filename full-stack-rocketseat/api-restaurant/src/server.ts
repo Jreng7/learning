fa878e7102a975bd771a5222b09b35d5a4e92429
Jreng7 <jr.waydev@gmail.com>
2025-02-09 19:08:58 -0300

    configurando knex em database e exportando ele para server.ts

--- full-stack-rocketseat/api-restaurant/src/server.ts
+++ full-stack-rocketseat/api-restaurant/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { routes } from "./routes/"
 import { errorHandler } from './middlewares/errorHandler'
+import { knex } from '../src/database/knex'
 
 
 const PORT = 3333
@@ -9,7 +10,7 @@ const server = express()
 server.use(express.json())
 server.use(routes)
 server.use(errorHandler)
-
+server.use(knex)
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`)
