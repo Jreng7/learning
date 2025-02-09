bce88ea0b33c238eca02224acddd54d5dda3289e
Jreng7 <jr.waydev@gmail.com>
2025-02-09 19:41:04 -0300

    types no database ProductRepository.d.ts

--- full-stack-rocketseat/api-restaurant/src/controllers/ProductController.ts
+++ full-stack-rocketseat/api-restaurant/src/controllers/ProductController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import { knex } from '@/database/knex'
 import { z } from 'zod'
 
 
@@ -17,10 +18,18 @@ export class ProductController {
   }
 
   async create(request: Request, response: Response, next: NextFunction){
-
+    
     try {
       
+      const bodySchema = z.object({
+        name: z.string({ required_error: "name is required!"}).trim().min(5),
+        price: z.number().gt(0, { message: "value must be greater than 0" })
+        
+      })
       
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex("products").insert()
 
       return response.status(201).json({ name, price })
       
