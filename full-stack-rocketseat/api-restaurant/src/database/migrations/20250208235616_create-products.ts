e1368f737dd3714df7a3c9f1a8330165227e3e45
Jreng7 <jr.waydev@gmail.com>
2025-02-09 01:04:10 -0300

    criada tabela na pasta banco de dados

--- full-stack-rocketseat/api-restaurant/src/database/migrations/20250208235616_create-products.ts
+++ full-stack-rocketseat/api-restaurant/src/database/migrations/20250208235616_create-products.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
 
   await knex.schema.createTable("products", (table) => {
-    
+
     table.increments("id").primary(),
     table.text("name").notNullable(),
     table.decimal("price").notNullable(),
@@ -15,5 +15,7 @@ export async function up(knex: Knex): Promise<void> {
 
 }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("products")
+} 
 
