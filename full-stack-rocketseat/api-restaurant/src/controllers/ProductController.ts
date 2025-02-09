9f72f267721ff22b2d50ac320cb00682710c8b07
Jreng7 <jr.waydev@gmail.com>
2025-02-09 19:22:08 -0300

    criar a rota create

--- full-stack-rocketseat/api-restaurant/src/controllers/ProductController.ts
+++ full-stack-rocketseat/api-restaurant/src/controllers/ProductController.ts
@@ -15,6 +15,16 @@ export class ProductController {
     }
 
   }
+
+  async create(request: Request, response: Response, next: NextFunction){
+
+    try {
+      
+    } catch (error) {
+      
+    }
+
+  }
 }
 
 
