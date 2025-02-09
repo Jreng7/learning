6d84d5f0be153307bb9ddbe30120755808c23d28
Jreng7 <jr.waydev@gmail.com>
2025-02-09 19:14:39 -0300

    zod instancia error

--- full-stack-rocketseat/api-restaurant/src/middlewares/errorHandler.ts
+++ full-stack-rocketseat/api-restaurant/src/middlewares/errorHandler.ts
@@ -1,9 +1,18 @@
-import { Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
 import { AppError } from '@/utils/AppError'
+import { Request, Response, NextFunction } from 'express'
 
 
 export function errorHandler(error: any, request: Request, response: Response, _: NextFunction){
-  if(error instanceof AppError){
+  
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message })
   }
+
+  if (error instanceof ZodError) {
+    
+  }
+
+
+  return response.status(500).json({ message: error.message })
 }
