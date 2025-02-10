57cc8ce314c981cae07866143345e3ecdf4a7f2a
Jreng7 <jr.waydev@gmail.com>
2025-02-10 12:44:18 -0300

    criando tables_sessions

---
+++ full-stack-rocketseat/api-restaurant/src/controllers/tables_sessions-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'
+
+export class TablesSessionsController {
+  async create(request: Request, response: Response, next: NextFunction){
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+  }
+}
