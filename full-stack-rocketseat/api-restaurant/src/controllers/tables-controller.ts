8487ee082e37c50ca6951ee1c725b0bd77ffc20f
Jreng7 <jr.waydev@gmail.com>
2025-02-10 00:48:48 -0300

    select().orderBy("table_number")

--- full-stack-rocketseat/api-restaurant/src/controllers/tables-controller.ts
+++ full-stack-rocketseat/api-restaurant/src/controllers/tables-controller.ts
@@ -1,40 +1,53 @@
-import { Request, Response, NextFunction } from 'express'
-import { AppError } from '@/utils/AppError'
 import { z } from 'zod'
 import { knex } from '@/database/knex'
+import { AppError } from '@/utils/AppError'
+import { Request, Response, NextFunction } from 'express'
+
 
 export class TablesController {
 
   async index(request: Request, response: Response, next: NextFunction){
+
     try {
-      
+      const tables = await knex("tables").select().orderBy("table_number")
+
     } catch (error) {
       next(error)
     }
+
   }
 
   async create(request: Request, response: Response, next: NextFunction){
+
     try {
+      const tables = await knex("tables").select()
       
     } catch (error) {
       next(error)
     }
+
   }
 
   async update(request: Request, response: Response, next: NextFunction){
+
     try {
+      const tables = await knex("tables").select()
       
     } catch (error) {
       next(error)
     }
+
   }
 
   async remove(request: Request, response: Response, next: NextFunction){
+
     try {
+      const tables = await knex("tables").select()
       
     } catch (error) {
       next(error)
     }
+
   }
 
 }
