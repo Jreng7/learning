0863bbc314c7bd3ae4270cb5f919603309175c48
Jreng7 <jr.waydev@gmail.com>
2025-02-10 00:14:13 -0300

    criar migration para tables (mesas)

--- full-stack-rocketseat/api-restaurant/src/database/seeds/insert-products.ts
+++ full-stack-rocketseat/api-restaurant/src/database/seeds/insert-products.ts
@@ -1,13 +1,21 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("table_name").del();
 
-    // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+    await knex("products").del();
+
+
+    await knex("products").insert([
+
+        { name: "Nhoque quatro queijos", price: 45 },
+        { name: "Isca de Frango", price: 60 },
+        { name: "Tilápia com alcaparras", price: 100 },
+        { name: "Bolinho de mandioca", price: 75 },
+        { name: "Caldo de palmito", price: 65 },
+        { name: "Strognoff de carne", price: 40 },
+        { name: "Pastel de pizza G", price: 24.80 },
+        { name: "Suco de laranja 1L", price: 17.50 },
+        { name: "Refrigerante Coca-Cola", price: 14.20 },
+        
     ]);
 };
