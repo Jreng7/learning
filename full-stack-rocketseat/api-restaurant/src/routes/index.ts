7fd749c4c5c30380822efae40202c3ae2c24ea7b
Jreng7 <jr.waydev@gmail.com>
2025-02-10 14:03:04 -0300

    projeto não finalizado devido a didática da rocketseat

--- full-stack-rocketseat/api-restaurant/src/routes/index.ts
+++ full-stack-rocketseat/api-restaurant/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 
-
 import { productsRoutes } from '@/routes/products-routes.js'
-import { tablesRoutes } from '@/routes/products-routes.js'
+import { tablesRoutes } from '@/routes/tables-routes'
+import { tablesSessions } from './tables_sessions-routes'
 
-
-export const routes = Router()
-
+const routes = Router()
 
 routes.use('/products', productsRoutes)
 routes.use('/tables', tablesRoutes)
+routes.use('/tables-sessions', tablesSessions)
+
+export { routes }
