148515b5d1f2b7f37e8c8e4d236f1fdd6347b969
Jreng7 <jr.waydev@gmail.com>
2025-02-11 15:31:19 -0300

    servidor terminado e vou tentar subir ele

--- hcode/nodejs_course/fundamentos01/server.js
+++ hcode/nodejs_course/fundamentos01/server.js
@@ -2,4 +2,15 @@ const { createServer } = require('node:http')
 const host = '127.0.0.1'
 const port = 3000
 
-const server = createServer
+const server = createServer((request, response) => {
+
+  response
+    .statusCode(200)
+    .setHeader('Content-Type', 'text/plain')
+    .end('Meu primeiro script')
+
+})
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`)
+})
