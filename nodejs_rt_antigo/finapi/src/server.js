c386bec6daf8a700d3e124431f1a7772d240a4af
Jreng7 <jr.waydev@gmail.com>
2025-02-13 22:06:56 -0300

    servidor criado com sucesso

--- nodejs_rt_antigo/finapi/src/server.js
+++ nodejs_rt_antigo/finapi/src/server.js
@@ -2,11 +2,18 @@ const express = require('express')
 const { config } = require('dotenv')
 
 config()
+
 const port = process.env.PORT || 3000
 const host = "127.0.0.1"
+
 const server =  express()
+server.use(express.json())
 
 
-server.listen(port, host, () => {
-
+server.listen(port, host, (err) => {
+  if(err){
+    console.error(err)
+  }
+  console.log(`Server running at port ${port}`)
 })
+
