22c27d8b4a2a54ffe3d4053be6e57f2a72b17b7a
Jreng7 <jr.waydev@gmail.com>
2025-02-14 23:02:13 -0300

    baixado uuid e percorrendo customers

--- nodejs_rt_antigo/finapi/src/server.js
+++ nodejs_rt_antigo/finapi/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { config } = require('dotenv')
+const { v4: uuidv4 } = require('uuid')
 
 config()
 
@@ -17,10 +18,18 @@ server.post('/account', (request, response) => {
   
   const { cpf, name } = request.body
 
-  const customerAlreadyExists = customers.some((item) => {
-    item.cpf === cpf
-    
-  }) 
+  const customerAlreadyExists = customers.some( itemPercorrido => { itemPercorrido.cpf === cpf } ) 
+
+  const id = uuidv4()
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+
 })
 
 server.listen(port, host, (err) => {
