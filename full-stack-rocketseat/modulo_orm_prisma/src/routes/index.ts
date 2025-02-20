75dae36fc083e500fe8fd18fe3e3f82ff89f7d19
Jreng7 <jr.waydev@gmail.com>
2025-02-19 22:04:49 -0300

    modulo main (index.ts) no routes

--- full-stack-rocketseat/modulo_orm_prisma/src/routes/index.ts
+++ full-stack-rocketseat/modulo_orm_prisma/src/routes/index.ts
@@ -3,8 +3,9 @@ import { Router } from "express"
 import { usersRoutes } from "./users-routes"
 import { questionsRoutes } from "./questions-routes"
 
-const routes = Router()
+export const routes = Router()
+
 routes.use("/questions", questionsRoutes)
 routes.use("/users", usersRoutes)
 
-export { routes }
+
