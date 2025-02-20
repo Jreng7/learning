61e0cb81b31adfafd01a82b8c02e2347406d2bab
Jreng7 <jr.waydev@gmail.com>
2025-02-20 19:33:36 -0300

    await prisma.user.create({ data: {} })

--- full-stack-rocketseat/modulo_orm_prisma/src/controllers/users-controller.ts
+++ full-stack-rocketseat/modulo_orm_prisma/src/controllers/users-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { prisma } from "@/database/prisma"
 
+
 export class UsersController {
 
   async index(request: Request, response: Response) {
@@ -8,8 +9,14 @@ export class UsersController {
   }
 
   async create(request: Request, response: Response) {
+
+    const { name, email } = request.body
+
+    await prisma.user.create({ data: {} })
+
     return response.status(201).json()
   }
+
   async show(request: Request, response: Response) {
     return response.json()
   }
