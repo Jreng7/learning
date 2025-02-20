952284e490e16e56b0f945ba8586539de86c9e71
Jreng7 <jr.waydev@gmail.com>
2025-02-19 22:59:58 -0300

    instalado biblioteca (async-erros) para tratamento

--- full-stack-rocketseat/modulo_orm_prisma/src/controllers/users-controller.ts
+++ full-stack-rocketseat/modulo_orm_prisma/src/controllers/users-controller.ts
@@ -8,11 +8,10 @@ export class UsersController {
 
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-      throw new Error("Broken")
+      throw new Error("Aqui fica o error.message")
       return response.status(201).json({ message: "ok"})
     } catch (error) {
-      console.error(error)
-      next()
+      next(error)
     }
   }
 
