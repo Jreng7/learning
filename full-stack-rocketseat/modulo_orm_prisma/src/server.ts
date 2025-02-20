78f18a34550b774fee777bbebae48e5e404aaa0c
Jreng7 <jr.waydev@gmail.com>
2025-02-19 23:05:16 -0300

    fazendo a importação do 'express-async-errors'

--- full-stack-rocketseat/modulo_orm_prisma/src/server.ts
+++ full-stack-rocketseat/modulo_orm_prisma/src/server.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response, NextFunction } from "express"
-import { routes } from "./routes"
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import { routes } from './routes'
 
 const PORT = 3333
 const app = express()
