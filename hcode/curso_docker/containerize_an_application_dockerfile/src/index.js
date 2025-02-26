ac99c513080f6c98143173d28dab868f59004a61
Jreng7 <jr.waydev@gmail.com>
2025-02-26 12:40:41 -0300

    aprendendo sobre containeriza an application

---
+++ hcode/curso_docker/containerize_an_application_dockerfile/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import os from 'node:os' // OS mostra as informações do Sistema Operacional do Usuário.
+
+const app = express()
+const port = 3333
+
+
+app.get('/', (req, res) => {
+  res.end(`Tipo do sistema: ${os.type()} Versao do Sistema: ${os.release()} Qual e a plataforma: ${os.platform()}`)
+})
+
+app.listen(port, () => {
+  console.log('Server listening on port 3333')
+})
