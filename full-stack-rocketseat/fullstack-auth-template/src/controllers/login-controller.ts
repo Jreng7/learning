d8af7e3f273ee692ad803c1f3efd2e1648fe9a32
Jreng7 <jr.waydev@gmail.com>
2025-03-02 20:43:36 -0300

    Realizando trataiva de Auth no login-controllers

--- full-stack-rocketseat/fullstack-auth-template/src/controllers/login-controller.ts
+++ full-stack-rocketseat/fullstack-auth-template/src/controllers/login-controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express"
 
+
+
 export class LoginController {
   async create(request: Request, response: Response) {
+
+    const { username, password } = request.body
+
+    const fakeUser = {
+      id: 1,
+      username: 'Miguel',
+      password: '123457'
+    }
+
+    if( username !== fakeUser.username || password !== fakeUser.password ) {
+      
+    }
+
     return response.json({ message: 'Okay' })
   }
 }
