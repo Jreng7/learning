bbe2f2015cd4f69eb38546c1430333f5a50e66f4
Jreng7 <jr.waydev@gmail.com>
2025-03-02 12:17:47 -0300

    instaldo jsonwebtoken

--- full-stack-rocketseat/fullstack-auth-template/src/controllers/login-controller.ts
+++ full-stack-rocketseat/fullstack-auth-template/src/controllers/login-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 
 export class LoginController {
   async create(request: Request, response: Response) {
-    return response.json({ message: process.env.USER_NAME })
+    return response.json({ message: 'Okay' })
   }
 }
 
