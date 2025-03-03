1c93cdc042a0972745c05d971f4746df03fc4587
Jreng7 <jr.waydev@gmail.com>
2025-03-03 18:14:17 -0300

    if (!authHeader) {     throw new AppError("Token JWT não encontado!", 401)   }

--- full-stack-rocketseat/fullstack-auth-template/src/middlewares/ensureAuthenticated.ts
+++ full-stack-rocketseat/fullstack-auth-template/src/middlewares/ensureAuthenticated.ts
@@ -1,10 +1,15 @@
+import { AppError } from '@/utils/AppError'
 import { Request, Response, NextFunction } from 'express'
 
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
 
   const authHeader = request.headers.authorization
 
-  console.log(authHeader)
+  if (!authHeader) {
+    throw new AppError("Token JWT não encontado!", 401)
+  }
+
+  const [, token] = authHeader.split(' ')
 
   return next()
 }
