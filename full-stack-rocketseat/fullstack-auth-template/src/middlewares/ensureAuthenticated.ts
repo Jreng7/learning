9daac449f505429fc7542b17e9d584bab9c8e843
Jreng7 <jr.waydev@gmail.com>
2025-03-03 18:05:58 -0300

    capturando authHeader

--- full-stack-rocketseat/fullstack-auth-template/src/middlewares/ensureAuthenticated.ts
+++ full-stack-rocketseat/fullstack-auth-template/src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 
-function ensureAuthenticated(){
-  
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
+
+  const authHeader = request.headers.authorization
+
+  console.log(authHeader)
+
+  return next()
 }
