e8d60e70ef067424f58d82dd91a4bd175ad8b04c
Jreng7 <jr.waydev@gmail.com>
2025-03-04 19:23:12 -0300

    import { Router } "destructuring"

--- b7web/fundamentos_nodejs/api_lista_contatos/src/app.ts
+++ b7web/fundamentos_nodejs/api_lista_contatos/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response } from 'express'
 import helmet from 'helmet'
 
 const app = express()
@@ -6,12 +6,7 @@ const app = express()
 // 1. Middlewares comuns primeiro (ex: helmet, cors, etc.)
 app.use(helmet())
 app.use(express.json())
-
-
-// 2. Rotas (ex: app.get, app.post...)
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
+app.use('/', router)
 
 
 export default app;
