a53093956bcccee37d443dbb4a38eb4ccec04d84
Jreng7 <jr.waydev@gmail.com>
2025-03-05 19:42:51 -0300

    array de objetos com zod

--- b7web/zod_nodejs/src/server.ts
+++ b7web/zod_nodejs/src/server.ts
@@ -3,7 +3,14 @@ import z from 'zod'
 const schema = z.object({
   name: z.string().min(2),
   email: z.string().email(),
-  age: z.number().min(18).max(120) // .min() do z.number() é diferente do .min() "string()" ele checa idade min e max.
+  age: z.number().min(18).max(120), // .min() do z.number() é diferente do .min() "string()" ele checa idade min e max.
+  status: z.boolean(),
+  characteristics: z.array(
+    z.object({
+      name: z.string(),
+      value: z.number()
+    })
+  )
 })
 
 
@@ -13,7 +20,11 @@ type User = z.infer<typeof schema>
 let data: User = {
   name: "John Doe",
   email: "johndoe@gmail.com",
-  age: 35
+  age: 35,
+  status: true,
+  characteristics: [
+    { name: 'Marca Ford', value: 150.000 }
+  ]
 }
 
 const resultado = schema.parse(data)
