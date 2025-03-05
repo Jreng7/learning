a8054b75edf3162704cd643bcb84bb4bdd1a1f6b
Jreng7 <jr.waydev@gmail.com>
2025-03-05 10:37:36 -0300

    mudança no index.ts

--- b7web/fundamentos_nodejs/api_lista_contatos/src/routes/index.ts
+++ b7web/fundamentos_nodejs/api_lista_contatos/src/routes/index.ts
@@ -3,5 +3,12 @@ import { Router, Request, Response } from 'express'
 export const router = Router()
 
 router.post('contato', (request: Request, response: Response) => {
-  
+
+    const { name } = request.body
+
+    if (!name) {
+      response.json({ error: 'O nome precisa ter pelo menos 2 caracteres.'})
+    }
+
+
 })
