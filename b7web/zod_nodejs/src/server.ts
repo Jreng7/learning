75aced2a8527f876811ff9a3420d24acddce768b
Jreng7 <jr.waydev@gmail.com>
2025-03-05 19:20:01 -0300

    montando objeto com dados

--- b7web/zod_nodejs/src/server.ts
+++ b7web/zod_nodejs/src/server.ts
@@ -1,4 +1,7 @@
 import z from 'zod'
 
-const schema = z.string()
-
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  age: z.number().min(18).max(120) // .min() do z.number() é diferente do .min() "string()" ele checa idade min e max.
+})
