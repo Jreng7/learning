b045a407b84e93eaa01bc90c3b76259a7da7a371
Jreng7 <jr.waydev@gmail.com>
2025-03-06 10:17:25 -0300

    fazendo a correção do "Than"

--- b7web/zod_nodejs/src/numbers.ts
+++ b7web/zod_nodejs/src/numbers.ts
@@ -1,16 +1,16 @@
 import z from 'zod'
 
-// Greater Then = GT  ( Maior que )
-// Lower Then = LT   ( Menor que )
-// Greater Then or Equal = GTE  ( Maior que ou igual a )
-// Lower Then or Equal = LTE
+// Greater Than = GT  ( Maior que )
+// Lower Than = LT   ( Menor que )
+// Greater Than or Equal = GTE  ( Maior que ou igual a )
+// Lower Than or Equal = LTE
 
 const schema = z.object({
 
   age: z.number().gt(17), // Tem que ter mais de 17 anos.
   age1: z.number().gte(18), // Aqui o GTE diz que precisa ter 18 ou mais!
   age2: z.number().lte(17) // Mesma coisa do GTE, porém, o LTE é para MENOR ou IGUAL ao número passado (determinado no z.lte() )
-  
+
 })
 
 type User = z.infer<typeof schema>
