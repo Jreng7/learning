d3d5acb12de7b49df3d9d949740429319bde3468
Jreng7 <jr.waydev@gmail.com>
2025-03-13 18:41:47 -0300

    execute e refreshToken

---
+++ nvprogramador_curso/mod_auth_auto/src/controllers/Auth.Controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+class AuthController {
+
+  async execute(req: Request, res: Response) {
+
+  }
+
+  async refreshtoken(req: Request, res: Response) {
+    
+  }
+
+}
+
+
+export default new AuthController()
