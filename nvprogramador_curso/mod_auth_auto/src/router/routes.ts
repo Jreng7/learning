fd00220729ca5e78033abd0c234a22b64822ef93
Jreng7 <jr.waydev@gmail.com>
2025-03-13 18:36:36 -0300

    configurando Auth.Controller.ts

--- nvprogramador_curso/mod_auth_auto/src/router/routes.ts
+++ nvprogramador_curso/mod_auth_auto/src/router/routes.ts
@@ -2,7 +2,12 @@ import { Router, Request, Response } from 'express';
 
 const routes = Router()
 
-routes.get('/', (request: Request, response: Response) => {
+routes.post('/auth', (request: Request, response: Response) => {
+  return response.status(201).json({ message: "Rota ok"})
+})
+
+
+routes.post('/auth/refreshtoken', (request: Request, response: Response) => {
   return response.status(201).json({ message: "Rota ok"})
 })
 
