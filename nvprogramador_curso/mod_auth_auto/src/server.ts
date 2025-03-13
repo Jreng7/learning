7586dd5948f5e1bef8ac074d32b20d186d32fc14
Jreng7 <jr.waydev@gmail.com>
2025-03-13 15:32:33 -0300

    configurando server.ts

---
+++ nvprogramador_curso/mod_auth_auto/src/server.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'; 
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet'; 
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json())
