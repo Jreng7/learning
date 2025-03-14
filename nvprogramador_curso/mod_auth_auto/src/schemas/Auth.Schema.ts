59a049ef5e1b4f1b825aca5b7c523e4ffb649e0f
Jreng7 <jr.waydev@gmail.com>
2025-03-14 17:06:24 -0300

    ajustes no schema

--- nvprogramador_curso/mod_auth_auto/src/schemas/Auth.Schema.ts
+++ nvprogramador_curso/mod_auth_auto/src/schemas/Auth.Schema.ts
@@ -2,5 +2,5 @@ import z from "zod";
 
 export const executeAuthSchema = z.object({
   email: z.string().email({ message: 'Formato de e-mail inválido.'}),
-  password: z.string().min(4).max(15).nonempty()
-})
+  password: z.string().min(4).max(15)
+}).strip()  // <--- Isso remove campos extras!
