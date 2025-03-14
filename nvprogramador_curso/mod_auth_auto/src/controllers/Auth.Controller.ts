5a557ee8cc1dceef9bc16715170bdec92e3b41bd
Jreng7 <jr.waydev@gmail.com>
2025-03-14 00:48:12 -0300

    estruturando controller primeiro

--- nvprogramador_curso/mod_auth_auto/src/controllers/Auth.Controller.ts
+++ nvprogramador_curso/mod_auth_auto/src/controllers/Auth.Controller.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express'
+import { executeAuthSchema } from '../schemas/Auth.Schema'
 
 class AuthController {
 
   async execute(req: Request, res: Response) {
+    try {
+      const validatedData = executeAuthSchema.parse(req.body);
 
+      if (!validatedData) {
+        return res.status(400).json({ error: "Erro email ou senha" });
+      }
+      
+      return res.status(201).json({ message: "Dados validados com sucesso!" });
+
+    } catch (err: any) {
+      res.status(400).json({ error: err.message })
+    }
   }
 
   async refreshtoken(req: Request, res: Response) {
--- nvprogramador_curso/mod_auth_auto/src/router/routes.ts
+++ nvprogramador_curso/mod_auth_auto/src/router/routes.ts
@@ -3,6 +3,9 @@ import AuthController from '../controllers/Auth.Controller';
 
 const routes = Router()
 
+routes.get('/', (req, res) => {
+  return res.status(200).json({ message: "server ok"})
+})
 routes.post('/auth', AuthController.execute)
 routes.post('/auth/refreshtoken', AuthController.refreshtoken)
 
